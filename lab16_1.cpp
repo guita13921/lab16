99bfc7b28f49b437970fd06f43a08f0b8d409811
guita13921 <guita13921@gmail.com>
2022-01-13 22:27:54 +0700

    lab16_2.cpp complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,31 @@
-include <iostream>
+#include<iostream>
 using namespace std;
+
+int a = 5;
+string b = "A";
+string &c = b;
+int *x = &a;
+string *y = &b;
+int **z =  &x;
+
 
 int main(){
 
-	
-	
+cout << a << " " << b << " " << c << " " << x << " " << y << " " << z;
+cout << endl;
+cout << &a << " " << &b << " " << &c << " " << &x << " " << &y << " " << &z;
+cout << endl;
+c = "F";
+cout << a << " " << b << " " << c << " " << x << " " << y << " " << z;
+cout << endl;
+*y = "W";
+cout << a << " " << b << " " << c << " " << x << " " << y << " " << z;
+cout << endl;
+*x = 6;
+cout << a << " " << b << " " << c << " " << x << " " << y << " " << z;
+cout << endl;
+**z = 7;
+cout << a << " " << b << " " << c << " " << x << " " << y << " " << z;
+cout << endl;
 	return 0;
 }
